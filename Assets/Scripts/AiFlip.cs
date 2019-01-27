d90a5a5d37610d646df3cc189c0758d380411da4
Manuel Saleta <menyuerudev@gmail.com>
2019-01-27 02:13:13 -0400

    Adding map design

---
+++ Assets/Scripts/AiFlip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiFlip : MonoBehaviour
+{
+    public float timeBetweenFlip;
+    private float LastFlip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LastFlip = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > LastFlip + timeBetweenFlip)
+        {
+            Flip();
+            LastFlip = Time.time;
+        }
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+}
---
+++ Assets/Scripts/PatrolAI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PatrolAI : MonoBehaviour
+{
+    private float LeftOffset;
+    private float RightOffset;
+
+    private Vector3 MoveToPosition;
+
+    public float speed;
+    public float timeBetweenFlip;
+
+    private float stoppedTime;
+    private bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LeftOffset = transform.Find("LeftOffset").transform.position.x;
+        RightOffset = transform.Find("RightOffset").transform.position.x;
+        MoveToPosition = transform.position;
+        MoveToPosition.x = RightOffset;
+        isMoving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, MoveToPosition, speed * Time.deltaTime);
+            if (transform.position == MoveToPosition)
+            {
+                isMoving = false;
+                stoppedTime = Time.time;
+            }
+        }
+        else if (Time.time > stoppedTime + timeBetweenFlip)
+        {
+            Flip();
+            isMoving = true;
+            MoveToPosition.x = transform.position.x == LeftOffset ? RightOffset : LeftOffset;
+        }
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(new Vector3(0, 180, 0));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(new Vector3(LeftOffset, transform.position.y), new Vector3(RightOffset, transform.position.y));
+    }
+}
